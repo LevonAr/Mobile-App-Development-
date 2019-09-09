123274f1610e9b791fef8f3118394601dc627682
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-09-08 22:36:56 -0700

    Update 1-closure.js

--- Javascript/note_1/1-closure.js
+++ Javascript/note_1/1-closure.js
@@ -15,6 +15,7 @@ const sayHello = makeHelloFunction()
 // the variable called message is not in scope here
 console.log('typeof message:', typeof message)
 
+// but the function sayHello still references a variable called message
 console.log(sayHello.toString())
 
 
