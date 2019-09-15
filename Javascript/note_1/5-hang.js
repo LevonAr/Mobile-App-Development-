9c8774b59869a658c903e09d6a8e24c64193710c
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-09-14 23:18:20 -0700

    Update 5-hang.js

--- Javascript/note_1/5-hang.js
+++ Javascript/note_1/5-hang.js
@@ -1,4 +1,4 @@
 // this function will freeze a browser page if run in console
 function hang(seconds = 5){
   const doneAt = Date.now() + seconds * 1000
-  while(Date.now()
+  while(Date.now() < doneAt)
