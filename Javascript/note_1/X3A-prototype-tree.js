959d52e1f0fc628f4e35d6cc5bf0efcb9a125b8f
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-09-26 21:44:04 -0700

    Update X3A-prototype-tree.js

--- Javascript/note_1/X3A-prototype-tree.js
+++ Javascript/note_1/X3A-prototype-tree.js
@@ -8,3 +8,22 @@ function FeatherWeight(){
     UFC_Fighter.call(this);
     this.weight = 145;
 }
+
+FeatherWeight.prototype = Object.create(UFC_Fighter.prototype)
+
+const Frankie_Edgar = new FeatherWeight
+
+UFC_Fighter.prototype.boss = "Dana White"
+
+Frankie_Edgar.boss
+
+//undefined
+
+// to my surprise it didn't work, so I went back and apparently its using const instead of var/let that 
+// fucked it up
+
+let Chad_Mendes = new FeatherWeight
+
+Chad_Mendes.boss
+
+// "Dana White"
