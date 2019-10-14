6abb05609202d2f23aa8549c96cf390e91f4ef46
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-13 23:37:21 -0700

    Update scraper.js

--- Javascript/HomeAdvisor/scraper.js
+++ Javascript/HomeAdvisor/scraper.js
@@ -4,3 +4,7 @@ from scrapy import Request
 
 class JobsSpider(scrapy.Spider):
     name = "jobs"
+    allowed_domains = ["craigslist.org"]
+    start_urls = ["https://newyork.craigslist.org/search/sks?"]
+
+    def parse(self, response):
