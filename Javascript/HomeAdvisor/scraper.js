4b5a2c77662f52973c8c80f7935d195b8e581da3
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-13 23:38:55 -0700

    Update scraper.js

--- Javascript/HomeAdvisor/scraper.js
+++ Javascript/HomeAdvisor/scraper.js
@@ -15,4 +15,5 @@ class JobsSpider(scrapy.Spider):
             absolute_url = response.urljoin(relative_url)
             title = job.xpath('a/text()').extract_first()
             address = job.xpath('span[@class="result-meta"]/span[@class="result-hood"]/text()').extract_first("")[2:-1]
-        
+            yield Request(absolute_url, callback=self.parse_page,
+                          meta={'URL': absolute_url, 'Title': title, 'Address': address})
