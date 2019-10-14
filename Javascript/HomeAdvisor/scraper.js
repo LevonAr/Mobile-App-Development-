d84de650e5a4a72a91b995e23b44bb174f250cf7
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-13 23:37:41 -0700

    Update scraper.js

--- Javascript/HomeAdvisor/scraper.js
+++ Javascript/HomeAdvisor/scraper.js
@@ -8,3 +8,6 @@ class JobsSpider(scrapy.Spider):
     start_urls = ["https://newyork.craigslist.org/search/sks?"]
 
     def parse(self, response):
+        jobs = response.xpath('//p[@class="result-info"]')
+
+        for job in jobs:
