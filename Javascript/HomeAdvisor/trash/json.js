cd71c69c1fb7340d9c752ce40f63263578de72e3
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-14 23:46:06 -0700

    Update json.js

--- Javascript/HomeAdvisor/trash/json.js
+++ Javascript/HomeAdvisor/trash/json.js
@@ -1,1 +1,15 @@
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../news_data.json', true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(JSON.parse(xobj.responseText));
+      }
+    };
+    xobj.send(null);  
+  }
 
+  loadJSON(function(json) {
+    console.log(json); 
+  });
