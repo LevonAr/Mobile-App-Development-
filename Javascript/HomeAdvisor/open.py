e6b6949a0c9a64f84731e27e42e29107830b0b8d
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-20 00:00:14 -0700

    Update open.py

--- Javascript/HomeAdvisor/open.py
+++ Javascript/HomeAdvisor/open.py
@@ -30,3 +30,31 @@ for i in data:
     counter = counter+1
 
 wb = Workbook() 
+
+
+sheet1 = wb.add_sheet('Sheet 1')
+
+for i in desc_dict:
+    pattern1 = "(\d{3}) (\d{3}) (\d{4})"
+    pattern2 = "(\d{3})-(\d{3})-(\d{4})"
+    pattern3 = "(\d{3}) (\d{3})-(\d{4})"
+    pattern4 = "(\d{3}) (\d{3})-(\d{4})"
+    pattern5 = "(\d{3})[)](\d{3})-(\d{4})"
+    pattern6 = "(\d{3})[)](\d{3}) (\d{4})"
+    pattern7 = "(\d{3})-(\d{7})"
+    pattern8 = "(\d{3}) (\d{7})"
+    pattern9 = "(\d{10})"
+    pattern10 = "(\d{3})[)] (\d{3})-(\d{4})"
+    pattern11 = "(\d{3})[)] (\d{3}) (\d{4})"
+
+    match1 = re.search(pattern1, desc_dict[i])
+    match2 = re.search(pattern2, desc_dict[i])
+    match3 = re.search(pattern3, desc_dict[i])
+    match4 = re.search(pattern4, desc_dict[i])
+    match5 = re.search(pattern5, desc_dict[i])
+    match6 = re.search(pattern6, desc_dict[i])
+    match7 = re.search(pattern7, desc_dict[i])
+    match8 = re.search(pattern8, desc_dict[i])
+    match9 = re.search(pattern9, desc_dict[i])
+    match10 = re.search(pattern10, desc_dict[i])
+    match11 = re.search(pattern11, desc_dict[i])
