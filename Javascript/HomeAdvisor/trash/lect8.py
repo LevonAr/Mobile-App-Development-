76241bc7a399502d2defc9c849ba7c8001cf3a12
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-22 00:00:26 -0700

    Update lect8.py

--- Javascript/HomeAdvisor/trash/lect8.py
+++ Javascript/HomeAdvisor/trash/lect8.py
@@ -6,8 +6,6 @@ db = SQL("sqlite:///lecture.db")
 
 rows = db.execute("SELECT * FROM Album WHERE Title = :t", t=sys.argv[1])
 
-# For each album in database
-
 for row in rows:
 
   # Print title of album
