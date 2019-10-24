7767ea3c19b44225a269702425cdd884a3e47e91
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-23 23:55:16 -0700

    Update lect8.py

--- Javascript/HomeAdvisor/trash/lect8.py
+++ Javascript/HomeAdvisor/trash/lect8.py
@@ -4,9 +4,4 @@ from cs50 import SQL
 
 db = SQL("sqlite:///lecture.db")
 
-rows = db.execute("SELECT * FROM Album WHERE Title = :t", t=sys.argv[1])
-
-for row in rows:
-
-  # Print title of album
-  print(row["Title"])
+rows = db.execute("SELECT * FROM Album WHERE Title = :t", t=sys.argv[1
