0c33632bd456fc3368babe6b6531c8df51246642
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-25 23:34:11 -0700

    Update letter.sql

--- Javascript/HomeAdvisor/trash/letter.sql
+++ Javascript/HomeAdvisor/trash/letter.sql
@@ -5,7 +5,7 @@ from cs50 import SQL
 db = SQL("sqlite:///lecture.db")
 
 # Query database for all albums
-rows = db.execute
+rows = db.execute("SELECT * FROM Album WHERE Title = :t", t=sys.argv[1])
 
 
 
