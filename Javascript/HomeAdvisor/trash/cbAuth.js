005d90510e87d4e87828def70a29b1932adf1e48
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-26 23:51:15 -0700

    Update cbAuth.js

--- Javascript/HomeAdvisor/trash/cbAuth.js
+++ Javascript/HomeAdvisor/trash/cbAuth.js
@@ -3,5 +3,9 @@ function login(req, res, callback)
     User.findOne({email: req.body.email}, function(err, user)
     {
         if (err) return callback(err)
+    
+        user.comparePassword(req.body.password, (err, isMatch) => 
+            {
+
     }
 }
