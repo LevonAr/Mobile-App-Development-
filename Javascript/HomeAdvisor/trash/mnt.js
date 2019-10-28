208a3ccd54ed53809ededc9f89eec0f39e147cee
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-27 23:53:20 -0700

    Update mnt.js

--- Javascript/HomeAdvisor/trash/mnt.js
+++ Javascript/HomeAdvisor/trash/mnt.js
@@ -1,3 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Constants } from 'expo';
+
+class Counter extends Component {
+  state = {
