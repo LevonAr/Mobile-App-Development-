97c50aac390d6721c31d79650ac724e01a16f8f8
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-28 23:31:41 -0700

    Update aaa.js

--- Javascript/HomeAdvisor/trash/aaa.js
+++ Javascript/HomeAdvisor/trash/aaa.js
@@ -1,4 +1,5 @@
 async function login(req, res, callback) {
   try {
     const user = await User.findOne({email: req.body.email})
+    const isMatch = await user.comparePassword(req.body.password)
     
