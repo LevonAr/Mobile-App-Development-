bedf66f6f513209568143c1664c8d60a6a0030fa
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-11-11 23:37:08 -0800

    Update lynch.js

--- Javascript/HomeAdvisor/trash/lynch.js
+++ Javascript/HomeAdvisor/trash/lynch.js
@@ -1,3 +1,6 @@
 function login(req, res, callback) {
   User.findOne({email: req.body.email}, function(err, user) {
     if (err) return callback(err)
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+    
