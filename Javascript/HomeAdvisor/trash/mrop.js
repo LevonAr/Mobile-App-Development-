72fe68b687fc62eb7b04f0f93f8866b351c16600
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-11-12 23:52:37 -0800

    Update mrop.js

--- Javascript/HomeAdvisor/trash/mrop.js
+++ Javascript/HomeAdvisor/trash/mrop.js
@@ -1,5 +1,4 @@
 function login(req, res, callback) {
-  User.findOne({email: req.body.email}, function(err, user) {
   User.findOne({email: req.body.email})
     .then(function(user) {
       return user.comparePassword(req.body.password)
