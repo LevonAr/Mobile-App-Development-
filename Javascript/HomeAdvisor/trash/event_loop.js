497554923894e97e729d973c53cec2ccd0aef675
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-11-13 23:23:48 -0800

    Update event_loop.js

--- Javascript/HomeAdvisor/trash/event_loop.js
+++ Javascript/HomeAdvisor/trash/event_loop.js
@@ -3,3 +3,7 @@ console.log('Message no. 1: Sync');
 setTimeout(function() {
    console.log('Message no. 2: setTimeout');
 }, 0);
+
+var promise = new Promise(function(resolve, reject) {
+   resolve();
+});
